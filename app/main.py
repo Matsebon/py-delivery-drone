7aa84a7041205bb90dca67293f5b9cf8936b1860
Anastasiia Matsebon <anastasiiamatsebon@gmail.com>
2025-03-19 15:31:45 +0100

    task

--- app/main.py
+++ app/main.py
@@ -42,7 +42,7 @@ class FlyingRobot(BaseRobot):
 class DeliveryDrone(FlyingRobot):
     def __init__(
             self, name: str, weight: int,
-            current_load: int, max_load_weight: int,
+            current_load: Cargo = None, max_load_weight: int,
             coords: list = None
     ) -> None:
         super().__init__(name, weight, coords)
